08bc85c15d7f4ca590b841fe6f8b6f3ee0670ca7
Noah E <nepstein10@gmail.com>
2026-01-13 23:07:23 -0500

    commit from new computer

--- parcel-test/my-react-app/src/pages/GuessPage.jsx
+++ parcel-test/my-react-app/src/pages/GuessPage.jsx
@@ -1,15 +1,83 @@
-import { VIDEO_HOST_LINK } from "../constants/textConstants";
-import { getRandomVideoId } from "../constants/videoDB";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import ControlBar from "../components/ControlBar";
+import GuessBar from "../components/GuessBar";
+import { VIDEO_HOST_LINK, API_URL } from "../constants/textConstants";
+// import { getRandomVideo, getVideoById } from "../constants/videoDB";
+// import { View, Text } from "react-native"
 
 function GuessPage() {
-  const randomId = getRandomVideoId()
-  console.log(randomId)
+  const [initialVideo, setInitialVideo] = useState(undefined)
+
+  const [activeVideo, setActiveVideo] = useState(undefined)
+  const [score, setScore] = useState(undefined)
+
+  useEffect(() => {
+    console.log("initial run", initialVideo)
+    if (!initialVideo) {
+      axios.get(`${API_URL}/videos/reduced`)
+      .then((response) => {
+        setInitialVideo(response?.data?.video)
+      })
+      .catch((err) => console.log(err))
+    }
+  }, [])
+
+  useEffect(() => {
+    console.log("setting active")
+    setActiveVideo(initialVideo)
+  }, [initialVideo])
+
+  // direction: "prev" or "next"
+  const switchVideo = (direction) => {
+    axios.get(`${API_URL}/videos/reduced`)
+    .then((response) => {
+      setActiveVideo(response?.data?.video)
+    })
+    .catch((err) => console.log(err))
+    // setActiveVideo(getVideoById(activeVideo?.[direction]));
+  }
+
+  const submitGuess = (guessDate) => {
+    console.log("Guess submitted: " + guessDate + "\nAnswer: " + initialVideo?.date);
+    const postBody = {
+      datestring: guessDate,
+      start_id: initialVideo?.id
+    }
+    axios.post(`${API_URL}/guess`, postBody)
+      .then((response) => setScore(response.data.score))
+      .catch((err) => console.log("ERROR", err))
+  }
+
+  
   return (
     <div>
+      {
+        console.log(activeVideo)}{
+        console.log(initialVideo)
+      } 
       <h1>
         When is this video from?
       </h1>
-      <iframe src={`${VIDEO_HOST_LINK}/${randomId}`} width="580" height="325"></iframe>
+      {
+        console.log(typeof(activeVideo), activeVideo, activeVideo?.id, activeVideo ? Object.keys(activeVideo) : null )
+      }
+      {
+        activeVideo?.id
+        ? <iframe src={`${VIDEO_HOST_LINK}/${activeVideo?.id}`} width="580" height="325" />
+        : <div width="580" height="325"><p>foobar</p></div>
+      }
+      <ControlBar
+        setFunc={switchVideo}
+        first={activeVideo?.prev == null}
+        last={activeVideo?.next == null}
+      />
+      <GuessBar submitGuess={submitGuess} />
+      { score && 
+      <div>
+        <p>Your guess scored: <span>{score}</span></p>
+      </div>
+      }
     </div>
   )
 }
--- parcel-test/my-react-app/src/pages/VideoHostPage.jsx
+++ parcel-test/my-react-app/src/pages/VideoHostPage.jsx
@@ -1,22 +1,35 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router";
-import { getVideoById } from "../constants/videoDB"
+import axios from "axios";
+import { API_URL } from "../constants/textConstants";
 
 export const VideoHostPage = () => {
   let { video_id } = useParams();
+
+  const [activeUrl, setActiveUrl] = useState("")
+
+  useEffect(() => {
+    if (video_id) {
+      console.log("sending with id: ", video_id)
+      axios.get(`${API_URL}/videos/url?id=${video_id}`)
+      .then((response) => {
+        setActiveUrl(response?.data?.url)
+      })
+      .catch((err) => console.log(err))
+    }
+  }, [video_id])
+
   console.log(video_id);
-  const video = getVideoById("" + video_id);
-  console.log(video)
-  
+
   return (
-    <VideoHostFrame video={video}/>
+    <VideoHostFrame url={activeUrl}/>
   )
 }
 
-const VideoHostFrame = ({video}) => {
-  console.log(video)
+const VideoHostFrame = ({url}) => {
   return (
     <>
-    {video && <iframe src={video.url} width="560" height="315"></iframe>}
+    {url && <iframe src={url} width="560" height="315"></iframe>}
     </>
   )
 }
